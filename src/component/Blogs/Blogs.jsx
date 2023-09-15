077a506c88bf1ac73ef99c43364cfb4467e71253
shaikot07 <saifulislamshaikat007@gmail.com>
2023-09-16 01:33:10 +0600

    fix some isuse

--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -4,9 +4,10 @@ import Blog from '../Blog/Blog';
 const Blogs = ({handleBookmark}) => {
       const [blogs,setBlogs]=useState([]);
       useEffect(() =>{
-            fetch('../../../public/data.json')
+            fetch('data.json')
             .then(res =>res.json())
             .then(data => setBlogs(data))
+            
       },[])
       return (
             <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 mt-8 sm:gap-0 md:gap-3 lg:ml-0 sm:ml-15 lg:gap-4 '>
--- src/component/Bookmarks/Bookmarks.jsx
+++ src/component/Bookmarks/Bookmarks.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Bookmark from '../Bookmark/Bookmark';
 
 const Bookmarks = ({bookmarks,totalCost,remainig}) => {
+      console.log(bookmarks);
       return (
             <div className='bg-white h-[400px] p-3 rounded-xl mt-8 lg:w-1/4 sm:w-full md:w-1/4'>
                   <h2 className='text-1xl font-bold text-sky-400'>Credit Hour Remaining {remainig} hr </h2>
